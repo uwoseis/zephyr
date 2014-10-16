5ae4ee6db11a293ba43b1668bc32d866ef92224a
Brendan Smithyman <brendan@bitsmithy.net>
2014-10-16 19:57:32 -0400

    Several changes towards implementing the PML. In particular, the buoyancies are set up in a different way and the divisors are separated out. Additionally, the padded arrays are now named as such to avoid accidentally using the wrong version.

--- zephyr/omegalike.py
+++ zephyr/omegalike.py
@@ -3,7 +3,8 @@ import SimPEG
 from SimPEG import Utils
 import scipy.sparse as sp
 
-DEFAULTBOUNDS = [False, False, False, False]
+DEFAULT_FREESURF_BOUNDS = [False, False, False, False]
+DEFAULT_PML_SIZE = 10
 
 # NOT CONVINCED THIS WORKS
 def setupBoundary (diagonals, freeSurf):
@@ -39,7 +40,7 @@ def setupBoundary (diagonals, freeSurf):
         else:
             diagonals[key][-1,:] = 0.
 
-def initHelmholtzNinePointCE (sc):
+def initHelmholtzNinePoint (sc):
 
     # Set up SimPEG mesh
     hx = np.ones(sc['nx']) * sc['dx']
@@ -68,13 +69,21 @@ def initHelmholtzNinePointCE (sc):
 
     # fast --> slow is x --> y --> z as Fortran
 
-    # Set up physical properties in matrices
-    omega = 2 * np.pi * sc['freq']
-    c = np.pad(c, pad_width=1, mode='edge')
-    rho = np.pad(rho, pad_width=1, mode='edge')
+    # Set up physical properties in matrices with padding
+    omega   = 2 * np.pi * sc['freq']
+    cPad    = np.pad(c, pad_width=1, mode='edge')
+    rhoPad  = np.pad(rho, pad_width=1, mode='edge')
 
     # Model parameter M
-    K = omega**2 / (c**2 * rho)
+    K = omega**2 / (cPad**2 * rhoPad)
+
+    # Horizontal, vertical and diagonal geometry terms
+    dx  = sc['dx']
+    dz  = sc['dz']
+    dxx = dx**2
+    dzz = dz**2
+    dxz = dx*dz
+    dd  = np.sqrt(dxz)
 
     # Visual key for finite-difference terms
     # (per Pratt and Worthington, 1990)
@@ -100,23 +109,42 @@ def initHelmholtzNinePointCE (sc):
         'CF':   (+1) * dims[0] + (+1),
     }
 
-    # Horizontal, vertical and diagonal geometry terms
-    dx  = sc['dx']
-    dz  = sc['dz']
-    dxx = dx**2
-    dzz = dz**2
-    dxz = 2*dx*dz
 
     # Buoyancies
-    bMM = 1. / rho[0:-2,0:-2] # bottom left
-    bME = 1. / rho[0:-2,1:-1] # bottom centre
-    bMP = 1. / rho[0:-2,2:  ] # bottom centre
-    bEM = 1. / rho[1:-1,0:-2] # middle left
-    bEE = 1. / rho[1:-1,1:-1] # middle centre
-    bEP = 1. / rho[1:-1,2:  ] # middle right
-    bPM = 1. / rho[2:  ,0:-2] # top    left
-    bPE = 1. / rho[2:  ,1:-1] # top    centre
-    bPP = 1. / rho[2:  ,2:  ] # top    right
+    bMM = 1. / rhoPad[0:-2,0:-2] # bottom left
+    bME = 1. / rhoPad[0:-2,1:-1] # bottom centre
+    bMP = 1. / rhoPad[0:-2,2:  ] # bottom centre
+    bEM = 1. / rhoPad[1:-1,0:-2] # middle left
+    bEE = 1. / rhoPad[1:-1,1:-1] # middle centre
+    bEP = 1. / rhoPad[1:-1,2:  ] # middle right
+    bPM = 1. / rhoPad[2:  ,0:-2] # top    left
+    bPE = 1. / rhoPad[2:  ,1:-1] # top    centre
+    bPP = 1. / rhoPad[2:  ,2:  ] # top    right
+
+    # Initialize averaged buoyancies on most of the grid
+    bMM = (bEE + bMM) / 2 # a2
+    bME = (bEE + bME) / 2 # d1
+    bMP = (bEE + bMP) / 2 # d2
+    bEM = (bEE + bEM) / 2 # a1
+    # ... middle
+    bEP = (bEE + bEP) / 2 # c1
+    bPM = (bEE + bPM) / 2 # f2
+    bPE = (bEE + bPE) / 2 # f1
+    bPP = (bEE + bPP) / 2 # c2
+
+    # Reset the buoyancies on the outside edges
+    bMM[ 0, :] = bEE[ 0, :]
+    bMM[ :, 0] = bEE[ :, 0]
+    bME[ 0, :] = bEE[ 0, :]
+    bMP[ 0, :] = bEE[ 0, :]
+    bMP[ :,-1] = bEE[ :,-1]
+    bEM[ :, 0] = bEE[ :, 0]
+    bEP[ :,-1] = bEE[ :,-1]
+    bPM[-1, :] = bEE[-1, :]
+    bPM[ :, 0] = bEE[ :, 0]
+    bPE[-1, :] = bEE[-1, :]
+    bPP[-1, :] = bEE[-1, :]
+    bPP[ :,-1] = bEE[ :,-1]
 
     # K = omega^2/(c^2 . rho)
     kMM = K[0:-2,0:-2] # bottom left
@@ -129,57 +157,39 @@ def initHelmholtzNinePointCE (sc):
     kPE = K[2:  ,1:-1] # top    centre
     kPP = K[2:  ,2:  ] # top    right
 
-    # Initialize outside edges
-    cMM = bEE.copy() / dxz # a2
-    cME = bEE.copy() / dzz # d1
-    cMP = bEE.copy() / dxz # d2
-    cEM = bEE.copy() / dxx # a1
-    # ... middle
-    cEP = bEE.copy() / dxx # c1
-    cPM = bEE.copy() / dxz # f2
-    cPE = bEE.copy() / dzz # f1
-    cPP = bEE.copy() / dxz # c2
-
-    # Reciprocal of the mass in each diagonal on the cell grid
-    cMM[1:  ,1:  ]  = (bEE[1:  ,1:  ] + bMM[1:  ,1:  ]) / (2 * dxz) # a2
-    cME[1:  , :  ]  = (bEE[1:  , :  ] + bME[1:  , :  ]) / (2 * dzz) # d1
-    cMP[1:  , :-1]  = (bEE[1:  , :-1] + bMP[1:  , :-1]) / (2 * dxz) # d2
-    cEM[ :  ,1:  ]  = (bEE[ :  ,1:  ] + bEM[ :  ,1:  ]) / (2 * dxx) # a1
-    # ... middle
-    cEP[ :  , :-1]  = (bEE[ :  , :-1] + bEP[ :  , :-1]) / (2 * dxx) # c1
-    cPM[ :-1,1:  ]  = (bEE[ :-1,1:  ] + bPM[ :-1,1:  ]) / (2 * dxz) # f2
-    cPE[ :-1, :  ]  = (bEE[ :-1, :  ] + bPE[ :-1, :  ]) / (2 * dzz) # f1
-    cPP[ :-1, :-1]  = (bEE[ :-1, :-1] + bPP[ :-1, :-1]) / (2 * dxz) # c2
-
     # 9-point fd star
-    acoef = 0.5461
-    bcoef = 0.4539
-    ccoef = 0.6248
-    dcoef = 0.09381
-    ecoef = 0.000001297
+    acoef   = 0.5461
+    bcoef   = 0.4539
+    ccoef   = 0.6248
+    dcoef   = 0.09381
+    ecoef   = 0.000001297
 
     # 5-point fd star
     # acoef = 1.0
     # bcoef = 0.0
     # ecoef = 0.0
 
+
+    # NB: bPM and bMP here are switched relative to S. Roecker's version
+    #     in OMEGA. This is because the labelling herein is always ?ZX.
+
     diagonals = {
-        'AD':   ecoef*kMM + bcoef*cMM,
-        'DD':   dcoef*kME + acoef*cME,
-        'CD':   ecoef*kMP + bcoef*cMP,
-        'AA':   dcoef*kEM + acoef*cEM,
-        'BE':   ccoef*kEE - acoef*(cEM+cEP+cME+cPE) - bcoef*(cMM+cPP+cMP+cPM),
-        'CC':   dcoef*kEP + acoef*cEP,
-        'AF':   ecoef*kPM + bcoef*cPM,
-        'FF':   dcoef*kPE + acoef*cPE,
-        'CF':   ecoef*kPP + bcoef*cPP,
+        'AD':   ecoef*kMM + bcoef*bMM / (2*dxz),
+        'DD':   dcoef*kME + acoef*bME / dzz,
+        'CD':   ecoef*kMP + bcoef*bMP / (2*dxz),
+        'AA':   dcoef*kEM + acoef*bEM / dxx,
+        'BE':   ccoef*kEE - acoef*(bEM/dxx+bEP/dxx+bME/dzz+bPE/dzz) - bcoef*(bMM+bPP+bMP+bPM)/(2*dxz),
+        'CC':   dcoef*kEP + acoef*bEP / dxx,
+        'AF':   ecoef*kPM + bcoef*bPM / (2*dxz),
+        'FF':   dcoef*kPE + acoef*bPE / dzz,
+        'CF':   ecoef*kPP + bcoef*bPP / (2*dxz),
     }
 
     # NOT CONVINCED THIS WORKS
     if 'freeSurf' in sc:
         setupBoundary(diagonals, sc['freeSurf'])
     else:
-        setupBoundary(diagonals, DEFAULTBOUNDS)
+        setupBoundary(diagonals, DEFAULT_FREESURF_BOUNDS)
 
     diagonals = np.array([diagonals[key].ravel() for key in keys])
     offsets = [offsets[key] for key in keys]
