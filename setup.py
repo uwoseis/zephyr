c81aa1f29d1def17a56f782e2d1cb260d97d719d
Brendan Smithyman <brendan@bitsmithy.net>
2015-03-09 14:51:39 -0400

    Cleanup setup.py a bit.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python
-"""Zephyr
-"""
+'''Zephyr
+'''
 
+import os
 from distutils.core import setup
 from setuptools import find_packages
-# from Cython.Build import cythonize
 import numpy as np
 
 CLASSIFIERS = [
-'Development Status :: 4 - Beta',
+'Development Status :: 3 - Alpha',
 'Intended Audience :: Developers',
 'Intended Audience :: Science/Research',
 'License :: OSI Approved :: MIT License',
@@ -23,30 +22,27 @@ CLASSIFIERS = [
 'Natural Language :: English',
 ]
 
-import os, os.path
-
-with open("README.md") as f:
-    LONG_DESCRIPTION = ''.join(f.readlines())
+with open('README.md') as fp:
+    LONG_DESCRIPTION = ''.join(fp.readlines())
 
 setup(
-    name = "Zephyr",
-    # version = "0.1.1",
+    name = 'Zephyr',
+    # version = '0.1.1',
     packages = find_packages(),
     install_requires = ['numpy>=1.7',
                         'scipy>=0.13',
                         'IPython>=2.3',
                        ],
-    author = "Brendan Smithyman",
-    author_email = "brendan@bitsmithy.net",
-    description = "Zephyr",
+    author = 'Brendan Smithyman',
+    author_email = 'brendan@bitsmithy.net',
+    description = 'Zephyr',
     long_description = LONG_DESCRIPTION,
-    license = "MIT",
-    keywords = "fullwaveform inversion",
-    # url = "",
-    download_url = "http://github.com/bsmithyman/zephyr",
-    classifiers=CLASSIFIERS,
-    platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    license = 'MIT',
+    keywords = 'full-waveform inversion',
+    # url = '',
+    download_url = 'http://github.com/bsmithyman/zephyr',
+    classifiers = CLASSIFIERS,
+    platforms = ['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
     use_2to3 = False,
     include_dirs=[np.get_include()],
-    # ext_modules = cythonize('someFiles.pyx')
 )
