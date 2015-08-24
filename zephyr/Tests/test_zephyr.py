1031fc93385dbcc465510011293c366f228a07ab
Brendan Smithyman <brendan@bitsmithy.net>
2015-08-24 15:46:00 -0400

    Parallel gradient computation and gradient test implemented.

--- zephyr/Tests/test_zephyr.py
+++ zephyr/Tests/test_zephyr.py
@@ -92,9 +92,90 @@ class TestZephyr(unittest.TestCase):
             for job in problem.forwardGraph.node[node].get('jobs', []):
                 self.assertTrue('error' not in job.status)
 
-    def test_DoSomething(self):
-        #self.requireParallel()
-        pass
+    def test_parallelGradient(self):
+
+        sc = self.getBaseConfig()
+        sc.update({
+            'freqs':    [1e2, 2e2, 3e2, 4e2],
+            'nky':      1,
+            'profile':  IPYPROFILE,
+        })
+
+        cTrue = sc['c']
+        cFlat = cTrue[0,0] * np.ones_like(cTrue)
+
+        from zephyr.Problem import SeisFDFD25DParallelProblem
+        from zephyr.Survey import SeisFDFD25DSurvey
+
+        problem = SeisFDFD25DParallelProblem(sc)
+        survey = SeisFDFD25DSurvey(sc['geom'])
+        survey.pair(problem)
+
+        problem.forward()
+
+        self.assertTrue(len(problem.dPred.keys()) == len(sc['freqs']))
+
+        problem.dObs = problem.dPred
+        problem.rebuildSystem(cFlat)
+
+        problem.forward()
+        self.assertTrue(problem.solvedF)
+
+        for node in problem.forwardGraph:
+            for job in problem.forwardGraph.node[node].get('jobs', []):
+                self.assertTrue('error' not in job.status)
+
+        problem.backprop()
+        self.assertTrue(problem.solvedB)
+
+        for node in problem.backpropGraph:
+            for job in problem.backpropGraph.node[node].get('jobs', []):
+                self.assertTrue('error' not in job.status)
+
+        normit = lambda x: np.sqrt(reduce(np.add, [np.sum(x[key]**2) for key in x])).real
+
+        gradient = problem.g
+        gradnorm = normit(gradient)
+        lastgradnorm = gradnorm
+
+        misfit = problem.misfit
+        mfnorm = normit(misfit)
+        lastmfnorm = mfnorm
+
+        self.assertTrue(len(problem.g.keys()) == len(sc['freqs']))
+        self.assertTrue(len(problem.misfit.keys()) == len(sc['freqs']))
+
+        mfformat = '%d\t%g\t%g'
+        print('#\tgradnorm\tmisfit')
+
+        for i in xrange(10):
+            wrongness = 0.5**i
+            c = wrongness*cFlat + (1-wrongness)*cTrue
+            problem.rebuildSystem(c)
+
+            problem.forward()
+            problem.backprop()
+
+            gradient = problem.g
+            gradnorm = normit(gradient)
+            misfit = problem.misfit
+            mfnorm = normit(misfit)
+
+            if i > 0:
+                self.assertTrue(gradnorm <= lastgradnorm)
+                self.assertTrue(mfnorm <= lastmfnorm)
+                misfit = normit(problem.misfit)
+
+            lastgradnorm = gradnorm
+            lastmfnorm = mfnorm
+
+            print(mfformat%(i, gradnorm, mfnorm))
+
+    # def test_parallelGradient(self):
+
+    # def test_DoSomething(self):
+    #     #self.requireParallel()
+    #     pass
 
 if __name__ == '__main__':
     unittest.main()
