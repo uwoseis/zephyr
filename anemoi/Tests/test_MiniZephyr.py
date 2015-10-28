d30557895e85996405d9171844697087e13dda3a
Brendan Smithyman <brendan@bitsmithy.net>
2015-10-28 21:04:26 +0000

    Write a new test that compares the MiniZephyr and AnalyticalHelmholtz solutions, ensuring the relative error is below 1%. Also, include a notebook that shows some of these comparisons more clearly.

--- anemoi/Tests/test_MiniZephyr.py
+++ anemoi/Tests/test_MiniZephyr.py
@@ -1,34 +1,66 @@
 import unittest
 import numpy as np
-from anemoi import MiniZephyr, SimpleSource
+from anemoi import MiniZephyr, SimpleSource, AnalyticalHelmholtz
 
 class TestMiniZephyr(unittest.TestCase):
+    
+    @staticmethod
+    def elementNorm(arr):
+        return np.sqrt((arr.conj()*arr).sum()) / arr.size
 
     def setUp(self):
         pass
 
-    def test_forwardModelling(self):
-
-        nx = 100
-        nz = 200
+    def test_cleanExecution(self):
 
         systemConfig = {
-            'dx':       1.,                             # m
-            'dz':       1.,                             # m
-            'c':        2500. * np.ones((nz,nx)),       # m/s
-            'rho':      1.    * np.ones((nz,nx)),       # density
-            'nx':       nx,                             # count
-            'nz':       nz,                             # count
-            'freeSurf': [False, False, False, False],   # t r b l
-            'nPML':     10,
+            'c':        2500.,                          # m/s
+            'rho':      1.,                             # density
+            'nx':       100,                            # count
+            'nz':       200,                            # count
             'freq':     2e2,
         }
+        
+        xs = 50
+        zs = 100
 
         Ainv = MiniZephyr(systemConfig)
         src = SimpleSource(systemConfig)
 
-        q = src(nx/2, nz/2)
+        q = src(xs, zs)
         u = Ainv*q
+    
+    def test_compareAnalytical(self):
+        
+        systemConfig = {
+            'c':        2500.,  # m/s
+            'rho':      1.,     # kg/m^3
+            'nx':       100,    # count
+            'nz':       200,    # count
+            'freq':     2e2,    # Hz
+        }
+        
+        xs = 25 
+        zs = 25
+
+        Ainv = MiniZephyr(systemConfig)
+        src = SimpleSource(systemConfig)
+        q = src(xs, zs)
+        uMZ = Ainv*q
+        
+        AH = AnalyticalHelmholtz(systemConfig)
+        uAH = AH(xs, zs)
+        
+        nx = systemConfig['nx']
+        nz = systemConfig['nz']
+        
+        uMZr = uMZ.reshape((nz, nx))
+        uAHr = uAH.reshape((nz, nx))
+        
+        error = self.elementNorm(uAHr[40:180,40:80] - uMZr[40:180,40:80])
+        
+        self.assertTrue(error < 1e-2)
+    
 
 if __name__ == '__main__':
     unittest.main()
