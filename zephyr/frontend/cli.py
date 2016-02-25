14b120b5ac3d1ba7f1c0f5ad5f99fcf70ae56739
Brendan Smithyman <brendan@bitsmithy.net>
2016-02-25 12:35:27 -0500

    Add initial example for Job layer.

--- zephyr/frontend/cli.py
+++ zephyr/frontend/cli.py
@@ -66,11 +66,18 @@ zephyr.add_command(migrate)
 
 @click.command()
 @click.argument('projnm')
-def model(projnm):
+@click.option('--job', default='OmegaJob', help='The job to run')
+@click.option('--count', default=1, help='Number of greetings.')
+def model(projnm, job):
     '''Run a forward model'''
-    
-    print('Running project!')
-    print('projnm: \t%s'%projnm)
+
+    from . import jobs
+
+    jClass = eval('jobs.%s'%(job,))
+    assert isinstance(jClass, jobs.Job)
+
+    j = jClass(projnm)
+    j.run()
 zephyr.add_command(model)
 
 @click.command()
