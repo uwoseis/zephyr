0ac1928b4a0cee96bb7f6a255729116e87089d5e
Brendan Smithyman <brendan@bitsmithy.net>
2016-03-04 13:01:37 -0500

    Update to example & whitespace cleanup in problem.py.

--- zephyr/middleware/problem.py
+++ zephyr/middleware/problem.py
@@ -9,22 +9,22 @@ from .fields import HelmFields
 EPS = 1e-15
 
 class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCache):
-    
+
     initMap = {
     #   Argument            Required    Rename as ...   Store as type
         'SystemWrapper':    (True,      None,           None),
     }
 
 #    maskKeys = []
-    
+
     surveyPair = HelmBaseSurvey
     cacheItems = ['_system']
-    
+
     def __init__(self, systemConfig, *args, **kwargs):
-         
+
         # Initialize anemoi side
-        BaseSCCache.__init__(self, systemConfig, *args, **kwargs)       
-        
+        BaseSCCache.__init__(self, systemConfig, *args, **kwargs)
+
         # Initialize SimPEG side
         hx = [(self.dx, self.nx-1)]
         hz = [(self.dz, self.nz-1)]
@@ -43,22 +43,22 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
 #            self._buildSystem()
 
     def updateModel(self, m, loneKey='c'):
-        
+
         if m is None:
             return
-        
+
         elif isinstance(m, dict):
             self.systemConfig.update(m)
             self.clearCache()
-        
+
         elif isinstance(m, np.ndarray) or isinstance(m, np.inexact) or isinstance(m, complex) or isinstance(m, float):
             if not np.linalg.norm(m - self.systemConfig.get(loneKey, 0.)) < EPS:
                 self.systemConfig[loneKey] = m
                 self.clearCache()
-        
+
         else:
                 raise Exception('Class %s doesn\'t know how to update with model of type %s'%(self.__class__.__name__, type(m)))
-            
+
     @property
     def system(self):
         if getattr(self, '_system', None) is None:
@@ -67,19 +67,19 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
 
     @SimPEG.Utils.timeIt
     def Jtvec(self, m=None, v=None, u=None):
-        
+
         if not self.ispaired:
             raise Exception('%s instance is not paired to a survey'%(self.__class__.__name__,))
-            
+
         if v is None:
             raise Exception('Actually, Jtvec requires a residual vector')
-        
+
         self.updateModel(m)
-        
+
         # v.shape <nrec,  nsrc,  nfreq>
         # o.shape [<nelem, nsrc> . nfreq]
         # r.shape [<nrec, nelem> . nsrc]
-                
+
         resid = v.reshape((self.survey.nrec, self.survey.nsrc, self.survey.nfreq))
         qb = self._getVSrcs(resid)
 
@@ -143,13 +143,13 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
 
 
     def fields(self, m=None):
-        
+
         uF = self._lazyFields(m)
         fields = HelmFields(self.mesh, self.survey)
-        
+
         for ifreq, uFsub in enumerate(uF):
             fields[:,'u',ifreq] = uFsub
-        
+
         return fields
 
 
@@ -165,17 +165,17 @@ class Helm2DProblem(HelmBaseProblem):
 
 
 class Helm2DViscoProblem(Helm2DProblem):
-    
+
     SystemWrapper = ViscoMultiFreq
 
-    
+
 class Helm25DProblem(HelmBaseProblem):
-    
+
     initMap = {
     #   Argument            Required    Rename as ...   Store as type
         'SystemWrapper':    (False,     None,           None),
     }
-    
+
     surveyPair = Helm25DSurvey
     SystemWrapper = MultiFreq
 
