365e7a583b733274ea831a6632e76ca3059760bc
Brendan Smithyman <brendan@bitsmithy.net>
2016-03-16 14:47:56 -0400

    Fix for one source vector per survey or scalar.

--- zephyr/middleware/problem.py
+++ zephyr/middleware/problem.py
@@ -9,22 +9,22 @@ from .fields import HelmFields
 EPS = 1e-15
 
 class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCache):
-    
+
     initMap = {
     #   Argument            Required    Rename as ...   Store as type
         'SystemWrapper':    (True,      None,           None),
     }
 
 #    maskKeys = []
-    
+
     surveyPair = HelmBaseSurvey
     cacheItems = ['_system']
-    
+
     def __init__(self, systemConfig, *args, **kwargs):
-         
+
         # Initialize anemoi side
-        BaseSCCache.__init__(self, systemConfig, *args, **kwargs)       
-        
+        BaseSCCache.__init__(self, systemConfig, *args, **kwargs)
+
         # Initialize SimPEG side
         hx = [(self.dx, self.nx-1)]
         hz = [(self.dz, self.nz-1)]
@@ -43,22 +43,22 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
 #            self._buildSystem()
 
     def updateModel(self, m, loneKey='c'):
-        
+
         if m is None:
             return
-        
+
         elif isinstance(m, dict):
             self.systemConfig.update(m)
             self.clearCache()
-        
+
         elif isinstance(m, np.ndarray) or isinstance(m, np.inexact) or isinstance(m, complex) or isinstance(m, float):
             if not np.linalg.norm(m - self.systemConfig.get(loneKey, 0.)) < EPS:
                 self.systemConfig[loneKey] = m
                 self.clearCache()
-        
+
         else:
                 raise Exception('Class %s doesn\'t know how to update with model of type %s'%(self.__class__.__name__, type(m)))
-            
+
     @property
     def system(self):
         if getattr(self, '_system', None) is None:
@@ -67,24 +67,24 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
 
     @SimPEG.Utils.timeIt
     def Jtvec(self, m=None, v=None, u=None):
-        
+
         if not self.ispaired:
             raise Exception('%s instance is not paired to a survey'%(self.__class__.__name__,))
-            
+
         if v is None:
             raise Exception('Actually, Jtvec requires a residual vector')
-        
+
         self.updateModel(m)
-        
+
         # v.shape <nrec,  nsrc,  nfreq>
         # o.shape [<nelem, nsrc> . nfreq]
         # r.shape [<nrec, nelem> . nsrc]
-                
+
         resid = v.reshape((self.survey.nrec, self.survey.nsrc, self.survey.nfreq))
-        
+
         if u is None:
             u = self._lazyFields(m)
-            
+
         # Make a list of receiver vectors for each frequency, each of size <nelem, nsrc>
         qb = [
               sp.hstack(
@@ -99,43 +99,43 @@ class HelmBaseProblem(SimPEG.Problem.BaseProblem, BaseModelDependent, BaseSCCach
               ) # <-- Sparse matrix of size <nelem, nsrc> constructed by hstack from generator
               for ifreq in xrange(self.survey.nfreq) # <-- Outer list of size <nfreq>
              ]
-        
+
         uB = self.system * qb
         if isinstance(u, HelmFields):
             g = reduce(np.add, ((u[:,'u',ifreq] * uBi).sum(axis=1) for ifreq, uBi in enumerate(uB)))
         else:
             g = reduce(np.add, ((uFi * uBi).sum(axis=1) for uFi, uBi in zip(u, uB)))
-        
+
         return g
-    
+
     def _lazyFields(self, m=None):
-        
+
         if not self.ispaired:
             raise Exception('%s instance is not paired to a survey'%(self.__class__.__name__,))
-        
+
         self.updateModel(m)
-        
+
         qs = self.survey.sVecs
-        
+
         if isinstance(self.survey.tsTerms, list) or isinstance(self.survey.tsTerms, np.ndarray):
-            qs = [qs * sp.diags(sterm.conjugate(),0) for sterm in self.survey.tsTerms]
+            qs = [qs * sp.diags(sterm.conjugate(),0) if np.iterable(sterm) else sterm.conjugate() * qs for sterm in self.survey.tsTerms]
         else:
-            qs = qs * sterm.conjugate()
+            qs = qs * self.survey.tsTerms.conjugate()
         uF = self.system * qs
-        
+
         if not np.iterable(uF):
             uF = [uF]
-        
+
         return uF
 
     def fields(self, m=None):
-        
+
         uF = self._lazyFields(m)
         fields = HelmFields(self.mesh, self.survey)
-        
+
         for ifreq, uFsub in enumerate(uF):
             fields[:,'u',ifreq] = uFsub
-        
+
         return fields
 
 
@@ -151,17 +151,17 @@ class Helm2DProblem(HelmBaseProblem):
 
 
 class Helm2DViscoProblem(Helm2DProblem):
-    
+
     SystemWrapper = ViscoMultiFreq
 
-    
+
 class Helm25DProblem(HelmBaseProblem):
-    
+
     initMap = {
     #   Argument            Required    Rename as ...   Store as type
         'SystemWrapper':    (False,     None,           None),
     }
-    
+
     surveyPair = Helm25DSurvey
     SystemWrapper = MultiFreq
 
