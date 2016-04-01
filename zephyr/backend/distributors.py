f15673cb819e0367b1b07c87dcf67763542d0cf5
Brendan Smithyman <brendan@bitsmithy.net>
2016-04-01 15:03:26 -0400

    Fix handling of Q in MultiFreq?

--- zephyr/backend/distributors.py
+++ zephyr/backend/distributors.py
@@ -15,7 +15,7 @@ else:
 PARTASK_TIMEOUT = None
 
 class BaseDist(DiscretizationWrapper):
-    
+
     initMap = {
     #   Argument        Required    Rename as ...   Store as type
         'Disc':         (True,      '_Disc',        None),
@@ -23,9 +23,9 @@ class BaseDist(DiscretizationWrapper):
         'nWorkers':     (False,     '_nWorkers',    np.int64),
         'remDists':     (False,     None,           list),
     }
-    
+
     maskKeys = {'remDists'}
-    
+
     @property
     def remDists(self):
         'Remaining distributor objects in the call graph'
@@ -36,19 +36,19 @@ class BaseDist(DiscretizationWrapper):
         if value:
             self._DiscOverride = value.pop(0)
         self._remDists = value
-    
+
     @property
     def Disc(self):
         'The discretization to instantiate'
 
         return getattr(self, '_DiscOverride', self._Disc)
-    
+
     @property
     def addFields(self):
         'Returns additional fields for the subProblem systemConfigs'
 
         return {'remDists': self.remDists}
-    
+
     @property
     def systemConfig(self):
         self._systemConfig.update(self.remDists)
@@ -59,15 +59,15 @@ class BaseDist(DiscretizationWrapper):
 
 
 class BaseMPDist(BaseDist):
-    
+
     maskKeys = {'parallel'}
-    
+
     @property
     def parallel(self):
-        'Determines whether to operate in parallel' 
+        'Determines whether to operate in parallel'
 
         return PARALLEL and getattr(self, '_parallel', True)
-    
+
     @property
     def pool(self):
         'Returns a configured multiprocessing Pool'
@@ -75,17 +75,17 @@ class BaseMPDist(BaseDist):
         if self.parallel:
             if not hasattr(self, '_pool'):
                 self._pool = multiprocessing.Pool(self.nWorkers)
-            return self._pool            
-            
+            return self._pool
+
         else:
             raise Exception('Cannot start parallel pool; multiprocessing seems to be unavailable')
-    
+
     @property
     def nWorkers(self):
         'Returns the configured number of parallel workers'
 
         return min(getattr(self, '_nWorkers', 100), self.cpuCount)
-    
+
     @property
     def cpuCount(self):
         'Returns the multiprocessing CPU count'
@@ -94,67 +94,67 @@ class BaseMPDist(BaseDist):
             return multiprocessing.cpu_count()
         else:
             return 1
-    
+
     @property
     def addFields(self):
         'Returns additional fields for the subProblem systemConfigs'
 
         fields = super(BaseMPDist, self).addFields
-        
+
         remCap = self.cpuCount / self.nWorkers
         if (self.nWorkers < self.cpuCount) and remCap > 1:
-            
+
             fields.update({'parallel': True, 'nWorkers': remCap})
-            
+
         return fields
-    
+
     def __mul__(self, rhs):
         '''
         Carries out the multiplication of the composite system
         by the right-hand-side vector(s).
-        
+
         Args:
             rhs (array-like or list thereof): Source vectors
-        
+
         Returns:
             u (iterator over np.ndarrays): Wavefields
         '''
-        
+
         if isinstance(rhs, list):
             getRHS = lambda i: rhs[i]
         else:
             getRHS = lambda i: rhs
-        
+
         if self.parallel:
             plist = []
             for i, sub in enumerate(self.subProblems):
-                
+
                 p = self.pool.apply_async(sub, (getRHS(i),))
                 plist.append(p)
-            
+
             u = (self.scaleTerm*p.get(PARTASK_TIMEOUT) for p in plist)
-            
+
         else:
             u = (self.scaleTerm*(sub*getRHS(i)) for i, sub in enumerate(self.subProblems))
-        
+
         return u
 
 
 class BaseIPYDist(BaseDist):
-    
+
     initMap = {
     #   Argument        Required    Rename as ...   Store as type
         'profile':      (False,     '_profile',     str),
     }
-    
+
     maskKeys = {'profile'}
-    
+
     @property
     def profile(self):
         'Returns the IPython parallel profile'
 
         return getattr(self, '_profile', 'default')
-    
+
     @property
     def pClient(self):
         'Returns the IPython parallel client'
@@ -171,7 +171,7 @@ class BaseIPYDist(BaseDist):
         if not hasattr(self, '_dView'):
             self._dView = self.pClient[:]
         return self._dView
-    
+
     @property
     def lView(self):
         'Returns a load-balanced view on the IPython parallel client'
@@ -179,7 +179,7 @@ class BaseIPYDist(BaseDist):
         if not hasattr(self, '_lView'):
             self._lView = self.pClient.load_balanced_view()
         return self._lView
-    
+
     @property
     def nWorkers(self):
         'Returns the configured number of parallel workers'
@@ -192,14 +192,14 @@ class MultiFreq(BaseMPDist):
     Wrapper to carry out forward-modelling using the stored
     discretization over a series of frequencies.
     '''
-    
+
     initMap = {
     #   Argument        Required    Rename as ...   Store as type
         'freqs':        (True,      None,           list),
     }
-    
+
     maskKeys = {'freqs'}
-    
+
     @property
     def spUpdates(self):
         'Updates for frequency subProblems'
@@ -259,7 +259,11 @@ class ViscoMultiFreq(MultiFreq):
         except TypeError:
             assert not self._any(criteria)
 
-        self._Q = value
+        if isinstance(value, np.ndarray):
+            assert value.size == self.nz*self.nx
+            self._Q = value.reshape((self.nz, self.nx))
+        else:
+            self._Q = value * np.ones((self.nz, self.nx), dtype=np.float64)
 
     @property
     def disperseFreqs(self):
@@ -303,13 +307,13 @@ class SerialMultiFreq(MultiFreq):
     discretization over a series of frequencies. Enforces
     serial execution.
     '''
-    
+
     @property
     def parallel(self):
-        'Determines whether to operate in parallel' 
+        'Determines whether to operate in parallel'
 
         return False
-    
+
     @property
     def addFields(self):
         'Returns additional fields for the subProblem systemConfigs'
